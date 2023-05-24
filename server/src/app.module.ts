81f26d0511bbb49e21f71cbdca6dc56ed75635bd
hishamac <122851090+hishamac@users.noreply.github.com>
2023-05-24 06:12:48 +0000

    saved

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,11 +4,17 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_URI),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql'
+    }),
     AuthModule
   ],
   controllers: [AppController],
